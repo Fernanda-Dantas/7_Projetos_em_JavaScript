cb14d288813ffa8039dda28ae5df629f2bbfff28
Fernanda Dantas <fer.dantas07@gmail.com>
2022-03-29 20:41:16 -0300

    códigos atualizados

--- typing_game/arquivos/comandos.js
+++ typing_game/arquivos/comandos.js
@@ -1,21 +1,17 @@
 window.addEventListener('load', init);
 
-// Níveis
 const levels = {
   easy: 5,
   medium: 3,
   hard: 2
 };
 
-// Mudar nível
-const currentLevel = levels.easy;
+const currentLevel = levels.medium;
 
-// Variáveis globais que serão usadas nas funções
 let time = currentLevel;
 let score = 0;
 let isPlaying;
 
-// ELEMENTOS DOM (HTML)
 const wordInput = document.querySelector("#word-input");
 const currentWord = document.querySelector("#current-word");
 const scoreDisplay = document.querySelector("#score");
@@ -23,36 +19,28 @@ const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
 
-// Array das palavras
 const words = [
-  'gato',
-  'cachorro',
-  'pato',
-  'marreco',
+  'hipopótamo',
+  'yanomami',
+  'vicissitude',
+  'quintessência',
   'enciclopédia',
   'lacraia',
-  'pesadelo',
+  'balaústre',
   'pseudônimo',
-  'investigar',
-  'estragar',
+  'hebdomadário',
+  'idiossincrasia',
   'endividamento'
 ];
 
-// Inicializar jogo
 function init() {
-  // Mostrar os números dos segundos de cada nível no HTML
   seconds.innerHTML = currentLevel;
-// Carregar palavras do array
 showWord(words);
-// Começar a colocar as palavras no input
 wordInput.addEventListener('input', startMatch);
-// Chama a contagem regressiva
 setInterval(countdown, 1000);
-// Checar o status do jogo
 setInterval(checkStatus, 50);
 };
 
-// Começar a partida
 function startMatch() {
   if(matchWords()) {
     isPlaying = true;
@@ -61,7 +49,6 @@ function startMatch() {
     wordInput.value = '';
     score++;
   } 
-  // se score for -1 mostra 0
   if (score === -1) {
     scoreDisplay.innerHTML = 0;
   } else {
@@ -69,7 +56,6 @@ function startMatch() {
   }
 };
 
-// Comparar as palavras
 function matchWords() {
   if(wordInput.value === currentWord.innerHTML) {
     message.innerHTML = 'A palavra está correta!';
@@ -82,29 +68,21 @@ function matchWords() {
 };
 
 function showWord(words) {
-  // Cria palavras do array randomicamente
   const randIndex = Math.floor(Math.random() * words.length);
-  // mostra a palavra random
   currentWord.innerHTML = words[randIndex];
 
 };
 
-// Função contagem regressiva
 function countdown() {
-  // checar se o tempo acabou
   if(time > 0) {
-    // Decrementa
     time--;
   } else if(time === 0) {
-    // Game over
     isPlaying = false;
   }
-  // Mostra o tempo
   timeDisplay.innerHTML = time;
 
 }
 
-// Checar status do jogo
 function checkStatus() {
   if(!isPlaying && time === 0) {
     message.innerHTML = 'Game over';
